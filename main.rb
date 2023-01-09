5deff8b1fb7bb6fb6e97d4680ea44e3cf6e2e6a8
Tung Nguyen <tongueroo@gmail.com>
2023-01-09 05:38:26 +0000

    add public docs

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 # NOTE: This repo contains only the documentation for the private BoltsOps repo code.
-# Original file: https://github.com/boltops-test/sg_child/blob/master/main.rb
+# Original file: https://github.com/boltops-test-learn/sg_child/blob/master/main.rb
 # The docs are publish so they are available for interested subscribers.
 # For access to the source code, you can become a BoltOps subscriber.
 # See: https://learn.boltops.com
